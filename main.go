39a2e1464a073d8a3104d6ae5c14799cf338ec6b
Bill Maxwell <bill@rancher.com>
2015-12-23 12:14:46 -0700

    add versioning support

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"os"
 
 	giddyupApp "github.com/cloudnautique/giddyup/app"
+	"github.com/cloudnautique/giddyup/version"
 	"github.com/codegangsta/cli"
 )
 
 func main() {
 	app := cli.NewApp()
+	app.Version = version.VERSION
 	app.Name = "giddyup"
 	app.Usage = "Entrypoint functions for Rancher"
 
